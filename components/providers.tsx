94d96df15a7270426ce080d9eab8f2e4db36ea02
BDunnCode <BDunnCode@github.com>
2024-02-01 18:09:19 -0600

    first commit eCommerce App

---
+++ components/providers.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+// Last project has CartProvider as well
+import { CartProvider } from "use-shopping-cart"
+
+import { Toaster } from "@/components/ui/toaster"
+import { TailwindIndicator } from "@/components/tailwind-indicator"
+import { ThemeProvider } from "@/components/theme-provider"
+
+interface Props {
+  children: React.ReactNode
+}
+
+export function Providers({ children }: Props) {
+  return <>{children}</>
+}
